ec74b333d46d6e5f5be6cddd3275a691a5a4d27f
Grafscorp <golyandringraf@gmail.com>
2025-06-30 15:11:11 +0300

    Update cubedetect.cpp

--- cubedetect/src/cubedetect.cpp
+++ cubedetect/src/cubedetect.cpp
@@ -1,5 +1,7 @@
 #include <cubedetect.hpp>
 
+namespace cubedetectcv{
+
 double lineAngleDegrees(const cv::Vec4i &line)
 {
     double dx = line[2] - line[0];
@@ -144,3 +146,219 @@ void extendLinesToIntersections(std::vector<cv::Vec4i>& lines, const std::vector
 
 
 
+cv::Mat detectAndDrawCube(cv::Mat& srcImage, bool vis_lines, bool vis_pointers, bool vis_vertex , bool vis_conture)
+{
+    #ifdef DEBUG
+    std::cout << "[v] Start proccesing image\n";
+    #endif
+    //Image preprocessing to optimize subsequent operations
+    #pragma region Preproccesing
+    cv::Mat cubeImage = srcImage.clone();
+    cv::Mat gray, blurred, edges;
+
+    // Конвертация в градации серого
+    cvtColor(srcImage, gray, cv::COLOR_BGR2GRAY);
+    
+    // Bыравнивание гистограммы для компенсации теней
+    cv::Mat claheImg;
+    cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(3.0, cv::Size(8, 8));
+    clahe->apply(gray, claheImg);
+    
+    // Гауссово размытие для уменьшения шума
+    GaussianBlur(claheImg, blurred, cv::Size(5, 5), 1.5);
+    
+    // Адаптивное пороговое преобразование 
+    cv::Mat thresh;
+    adaptiveThreshold(blurred, thresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, 
+                     cv::THRESH_BINARY, 11, 2);
+
+    Canny(thresh, edges, THRESHOLD1_CANNY, THRESHOLD2_CANNY);
+
+    // Морфологические операции для улучшения линий
+    cv::Mat kernel_dilate = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+    dilate(edges, edges, kernel_dilate);
+    
+    cv::Mat kernel_erode = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 1));
+    erode(edges, edges, kernel_erode);
+    
+    #pragma endregion Preproccesing
+
+
+    //Tracing the contours of an image with lines
+    #pragma region ComputeLines
+    #ifdef DEBUG
+    std::cout << "[v] Start compute line\n";
+    #endif
+    std::vector<cv::Vec4i> lines;
+    HoughLinesP(edges, lines,
+        RHO_HOUGHLINES, THETA_HOUGHLINES, THRESHOLD_HOUGHLINES, 
+        MINLINELENGTH_HOUGHLINES, MAXLINEGAP_HOUGHLINES);  
+    
+
+    std::vector<cv::Vec4i> filteredLines;
+    for (const auto& line : lines) {
+        double length = cv::norm(cv::Point2f(line[0], line[1]) - cv::Point2f(line[2], line[3]));
+       
+        if (length >  FILTRED_LENGHTLINE) {
+            filteredLines.push_back(line);
+        }
+    }
+
+
+
+    std::vector<double> angles;
+    for (const auto& line : filteredLines) {
+        // Вычисляем угол для каждой линии
+        angles.push_back(lineAngleDegrees(line));
+    }
+        #ifdef DEBUG
+    std::cout << "[v] line angle degress is completed\n";
+    #endif
+    #pragma endregion ComputeLines
+    
+    #pragma region Clusterring
+    // Ожидаем 3 основных направления для куба
+    cv::Mat anglesMat(angles.size(), 1, CV_32F);
+    for (size_t i = 0; i < angles.size(); i++) {
+        anglesMat.at<float>(i) = static_cast<float>(angles[i]);
+    }
+
+    cv::Mat labels, centers;
+    // Применяем алгоритм k-means для кластеризации линий
+    kmeans(anglesMat, CLUSTERCOUNT, labels, 
+            cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1.0),
+            3, cv::KMEANS_PP_CENTERS, centers);
+
+    std::vector<cv::Point2f> intersections;
+    for (size_t i = 0; i < filteredLines.size(); i++) {
+        for (size_t j = i + 1; j < filteredLines.size(); j++) {
+            if (labels.at<int>(i) != labels.at<int>(j)) {
+                cv::Point2f pt = linesIntersection(filteredLines[i], filteredLines[j]);
+                
+                if (pt.x >= 0 && pt.y >= 0 && pt.x < srcImage.cols && pt.y < srcImage.rows) {
+                    double dist1 = std::min(norm(pt - cv::Point2f(filteredLines[i][0], filteredLines[i][1])), 
+                                    cv::norm(pt - cv::Point2f(filteredLines[i][2], filteredLines[i][3])));
+                    double dist2 = std::min(cv::norm(pt - cv::Point2f(filteredLines[j][0], filteredLines[j][1])), 
+                                    cv::norm(pt - cv::Point2f(filteredLines[j][2], filteredLines[j][3])));
+                    
+                    // Допуск для теневых областей
+                    if (dist1 < 45 || dist2 < 45) {
+                        intersections.push_back(pt);
+                    }
+                }
+            }
+        }
+    }
+        #ifdef DEBUG
+    std::cout << "[v] lines Intersection is completed\n";
+    #endif
+
+    
+    std::vector<cv::Point2f> vertices = clusterPoints(intersections, 25.0f);  
+        #ifdef DEBUG
+    std::cout << "[v] clustering is completed\n";
+    #endif
+
+    extendLinesToIntersections(filteredLines, vertices, 50.0f);  
+            #ifdef DEBUG
+        std::cout << "[v] extendLinesToIntersections is completed\n";
+        #endif
+    #pragma endregion 
+
+    #pragma region Vertex
+            #ifdef DEBUG
+    std::cout << "[v] Start compute vertex\n";
+    #endif
+    std::vector<cv::Point2f> filteredVertices;
+    for (const auto& vertex : vertices) {
+        int connections = 0;
+        
+        for (const auto& line : filteredLines) {
+            double dist = distanceToLine(vertex, line);
+            // Допуск расстояния
+            if (dist < 15.0) {
+                connections++;
+            }
+        }
+        
+        // Порог соединений для теневых областей
+        if (connections >= 1) {  
+            filteredVertices.push_back(vertex);
+        }
+    }
+        #ifdef DEBUG
+    std::cout << "[v] Distance to line is completed\n";
+    #endif
+    #pragma endregion
+
+    #pragma region Countour
+            #ifdef DEBUG
+    std::cout << "[v] Start compute countour\n";
+    #endif
+    std::vector<cv::Point> cubeContour;
+    if (filteredVertices.size() >= 4) {
+        std::vector<cv::Point2f> hullPoints;
+        convexHull(filteredVertices, hullPoints);
+        
+        // аппроксимация контура
+        std::vector<cv::Point2f> approxHull;
+        approxPolyDP(hullPoints, approxHull, 8.0, true); 
+        
+        // Проверка и коррекция для неполных кубов
+        if (approxHull.size() < 4) {
+            // Если вершин меньше 4, используем все обнаруженные точки
+            for (const auto& pt : filteredVertices) {
+                cubeContour.push_back(cv::Point(cvRound(pt.x), cvRound(pt.y)));
+            }
+        } else {
+            for (const auto& pt : approxHull) {
+                cubeContour.push_back(cv::Point(cvRound(pt.x), cvRound(pt.y)));
+            }
+        }
+    }
+    #pragma endregion 
+    
+    #pragma region Visualization
+            #ifdef DEBUG
+    std::cout << "[v] Start compute visualization\n";
+    #endif
+    // if(vis_lines)
+    // {
+    //     for (const auto& line : filteredLines) {
+    //         cv::line(cubeImage, cv::Point(line[0], line[1]), 
+    //                 cv::Point(line[2], line[3]), cv::Scalar(0, 180, 0), 2);
+    // }}
+    // if(vis_pointers)
+    // {
+    //     for (const auto& pt : intersections) {
+    //         cv::circle(cubeImage, pt, 3, cv::Scalar(255, 0, 0), -1);
+    // }}
+
+    // if(vis_vertex)
+    // {
+    // for (const auto& vertex : filteredVertices) {
+    //     cv::circle(cubeImage, vertex, 6, cv::Scalar(0, 0, 255), -1);
+    // }}
+
+    if(vis_conture){
+        if (!cubeContour.empty()) {
+            // Рисуем линии контура
+            for (size_t i = 0; i < cubeContour.size(); i++) {
+                cv::line(cubeImage, cubeContour[i], cubeContour[(i+1) % cubeContour.size()], 
+                        cv::Scalar(255, 0, 255), 3);
+            }
+            
+            // Добавляем прозрачную заливку
+            cv::Mat overlay = cubeImage.clone();
+            cv::fillPoly(overlay, std::vector<std::vector<cv::Point>>{cubeContour}, cv::Scalar(255, 0, 255));
+            cv::addWeighted(overlay, 0.2, cubeImage, 0.8, 0, cubeImage);
+        }
+        }
+    #pragma endregion
+            #ifdef DEBUG
+    std::cout << "[v] Complete detect Cube\n";
+    #endif
+    return cubeImage;
+    }
+    
+}
