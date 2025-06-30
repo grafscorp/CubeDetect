bc038d272283a10eb8dc2710b2cbad9ccace714e
Grafscorp <golyandringraf@gmail.com>
2025-06-30 15:10:26 +0300

    Create LinesIntersections

--- cubedetect/src/cubedetect.cpp
+++ cubedetect/src/cubedetect.cpp
@@ -10,3 +10,30 @@ double lineAngleDegrees(const cv::Vec4i &line)
 }
 
 
+cv::Point2f linesIntersection(const cv::Vec4i &line1, const cv::Vec4i &line2)
+{
+    //Get coords ends of the lines
+    float x1 = line1[0], y1 = line1[1];
+    float x2 = line1[2], y2 = line1[3];
+    float x3 = line2[0], y3 = line2[1];
+    float x4 = line2[2], y4 = line2[3];
+
+    //Denominator for equation lines
+    float denom = (y4 - y3)*(x2 - x1) - (x4 - x3)*(y2 - y1);
+
+    /*If lines is parallel 
+    @var denom is close to zero
+    */
+    if(fabs(denom) < 1e-5) return cv::Point2f(-1,-1);
+
+    float ua = ((x4 - x3)*(y1 - y3) - (y4 - y3)*(x1 - x3)) / denom;
+
+    return cv::Point2f(
+        x1 + ua*(x2-x1),
+        y1+ua*(y2-y1)
+    );
+
+
+
+}
+
